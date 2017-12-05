2cf1278b9c08cf92a637abdd5c57689b291e45d7
Sarina <33553117+SarinaKarmacharya@users.noreply.github.com>
2017-12-05 10:22:25 -0500

    Update gk_age.py

--- gk_age.py
+++ gk_age.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Jul 21 09:18:05 2017
 
-@author: vs796
+@author: sarina karmacharya
 """
 
 from scipy.stats.stats import pearsonr
@@ -17,14 +17,14 @@ import pandas as pd
 gk_array_adhd = []
 
 
-caselist_adhd = open("/rfanfs/pnl-zorro/home/vidushi/ADHD_MSD_FW/caselist_ADHD.csv",'r+')
+caselist = open("caselist.csv",'r+')
 
 
-for line in caselist_adhd:
+for line in caselist:
     casenumber = line.rstrip()
     
     
-    image_data_gk, image_header_gk = load('/rfanfs/pnl-zorro/projects/ADHD/MultiGaussian_NoV2016/GMM_07292017/GK/{0}_GK.nii' .format(casenumber))
+    image_data_gk, image_header_gk = load('{0}_GK.nii' .format(casenumber))
     
     vector_gk = np.reshape(image_data_gk, [np.prod(np.array(image_data_gk.shape))])
 
@@ -33,18 +33,18 @@ for line in caselist_adhd:
     gk_array_adhd.append(average_gk)
 
 
-caselist_adhd.close()
+caselist.close()
 
-age_array_adhd = []
+age_array = []
 
-agelist_adhd = open("/rfanfs/pnl-zorro/home/vidushi/ADHD_MSD_FW/age_adhd.csv",'r+')
+agelist = open("age.csv",'r+')
 
-for line in agelist_adhd:
+for line in agelist:
     age =  line.rstrip()
     
-    age_array_adhd.append(float(age))
+    age_array.append(float(age))
     
-agelist_adhd.close()
+agelist.close()
 
 
 #########
@@ -52,14 +52,14 @@ agelist_adhd.close()
 gk_array = []
 
 
-caselist = open("/rfanfs/pnl-zorro/home/vidushi/ADHD_MSD_FW/caselist_control.csv",'r+')
+caselist = open("caselist_control.csv",'r+')
 
 
 for line in caselist:
     casenumber = line.rstrip()
     
     
-    image_data_gk, image_header_gk = load('/rfanfs/pnl-zorro/projects/ADHD/MultiGaussian_NoV2016/GMM_07292017/GK/{0}_GK.nii' .format(casenumber))
+    image_data_gk, image_header_gk = load('{0}_GK.nii' .format(casenumber))
     
     vector_gk = np.reshape(image_data_gk, [np.prod(np.array(image_data_gk.shape))])
 
@@ -72,7 +72,7 @@ caselist.close()
 
 age_array = []
 
-agelist = open("/rfanfs/pnl-zorro/home/vidushi/ADHD_MSD_FW/age_control.csv",'r+')
+agelist = open("age_control.csv",'r+')
 
 for line in agelist:
     age =  line.rstrip()
@@ -85,7 +85,7 @@ agelist.close()
 data={'GK': gk_array,'AGE': age_array }
 d = pd.DataFrame(data)
 
-data1={'GK ADHD': gk_array_adhd, 'AGE ADHD': age_array_adhd}
+data1={'GK': gk_array, 'AGE ': age_array}
 d1 = pd.DataFrame(data1)
 
 
@@ -103,7 +103,7 @@ t, s = (pearsonr(age_array_adhd, gk_array_adhd))
 
 sns.regplot(x="AGE", y="GK", robust=True, data=d, ci = None, scatter_kws = {'color':'red'}, line_kws = {'color':'red'})
 
-sns.regplot(x="AGE ADHD", y="GK ADHD", robust=True, data=d1, ci = None, scatter_kws = {'color':'blue'}, line_kws = {'color':'blue'})
+sns.regplot(x="AGE", y="GK ", robust=True, data=d1, ci = None, scatter_kws = {'color':'blue'}, line_kws = {'color':'blue'})
     
 
 sns.plt.ylabel("General Kurtosis", size = 16)
@@ -114,10 +114,10 @@ green_line = mpatches.Patch(color = 'red')
     
 blue_line = mpatches.Patch(color = 'blue')
 
-plt.legend([green_line, blue_line], ['Control = {0}' .format(z), 'ADHD = {0}' .format(t)], prop = {'size': 12})
+plt.legend([green_line, blue_line], ['Control = {0}' .format(z), 'A = {0}' .format(t)], prop = {'size': 12})
 
     
-#plt.savefig('/rfanfs/pnl-zorro/home/vidushi/ADHD_MSD_FW/final_graphs/correlation_age_gk.png', bbox_inches = 'tight')
+
     
     
 sns.plt.show()
